11e3d0faed370c70a22a019711ef5c4e764e39fc
Gowtham KK <gowtham.kk@moengage.com>
2023-09-29 13:58:57 +0530

    feat: some feature BREAKING CHANGES: Some Change

--- packages/pulse_auth/pulse_auth/lib/pulse_auth.dart
+++ packages/pulse_auth/pulse_auth/lib/pulse_auth.dart
@@ -5,6 +5,7 @@ PulseAuthPlatform get _platform => PulseAuthPlatform.instance;
 /// Returns the name of the current platform.
 Future<String> getPlatformName1() async {
   final platformName = await _platform.getPlatformName();
-  if (platformName == null) throw Exception('Unable to get platform name.');
+  if (platformName == null)
+    throw Exception('Unable to get platform name.');
   return platformName;
 }
