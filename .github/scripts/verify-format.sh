d16eaeef3db7a6c4399608db9afded97a03269dd
Gowtham KK <gowtham.kk@moengage.com>
2024-09-18 15:13:46 +0530

    Update command

---
+++ .github/scripts/verify-format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd "$(dirname "$0")/.." || exit
+unformatted_files=$(git ls-files --modified)
+
+# Check if there are any modified (unformatted) files
+if [[ -n $unformatted_files ]]; then
+  echo "The following files are not formatted correctly:"
+  echo "$unformatted_files" # This will list the file names
+
+  # Optionally checkout the files to discard changes if running in CI
+  if [[ $GITHUB_WORKFLOW ]]; then
+    git checkout . &> /dev/null
+  fi
+
+  echo "To fix these files locally, run: 'melos run format'"
+  exit 1
+else
+  echo "✅ All files are formatted correctly."
+fi
