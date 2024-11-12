4c27bd901408bb0cf28f3272d217738cfc10d64d
Gowtham KK <gowtham.kk@moengage.com>
2024-11-12 17:15:46 +0530

    Update File path

--- .github/scripts/release.main.kts
+++ .github/scripts/release.main.kts
@@ -1,6 +1,5 @@
 #!/usr/bin/env kotlin
 
-@file:Import("../../../../sdk-automation-scripts/scripts/release/flutter-release.main.kt")
-@file:Import("../../../../sdk-automation-scripts/scripts/utils/common-utils.kt")
+@file:Import("../../../sdk-automation-scripts/scripts/release/flutter-release.main.kts")
 
 releasePlugins()
