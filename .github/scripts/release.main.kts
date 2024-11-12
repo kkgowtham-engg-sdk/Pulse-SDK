bf5df4f378b703254b71e7a6b9519b1bf440605b
Gowtham KK <gowtham.kk@moengage.com>
2024-11-12 16:44:06 +0530

    Release updates

--- .github/scripts/release.main.kts
+++ .github/scripts/release.main.kts
@@ -1,18 +1,6 @@
 #!/usr/bin/env kotlin
 
-@file:Import("../../../sdk-automation-scripts/scripts/utils/git-utils.kt")
+@file:Import("../../../sdk-automation-scripts/scripts/release/flutter-release.main.kt")
 @file:Import("../../../sdk-automation-scripts/scripts/utils/common-utils.kt")
-
-fun releasePlugins(){
-    println("Merge development branch to master")
-    mergeDevBranchToMaster()
-    println("Publish plugins")
-    executeCommandOnShell("melos publish --no-dry-run --git-tag-version --yes")
-    println("Merge master branch to development")
-    mergeMasterToDevBranch()
-    println("Publish local tags")
-    pushLocalTags()
-    println("Published plugins")
-}
 
 releasePlugins()
