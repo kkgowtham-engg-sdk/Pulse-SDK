c43998873a2997eeabdda3d83ddf414b7a8c58a1
Gowtham KK <gowtham.kk@moengage.com>
2024-11-12 14:44:31 +0530

    Release Plugins

---
+++ .github/scripts/release.main.kts
@@ -0,0 +1,18 @@
+#!/usr/bin/env kotlin
+
+@file:Import("../../../sdk-automation-scripts/scripts/utils/git-utils.kt")
+@file:Import("../scripts/utils.main.kts")
+
+fun releasePlugins(){
+    println("Merge development branch to master")
+    mergeDevBranchToMaster()
+    println("Publish plugins")
+    executeCommandOnShell("melos publish --no-dry-run --git-tag-version --yes")
+    println("Merge master branch to development")
+    mergeMasterToDevBranch()
+    println("Publish local tags")
+    pushLocalTags()
+    println("Published plugins")
+}
+
+releasePlugins()
